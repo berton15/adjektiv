f6ad108d2b31d010235376c10eb031396bc81313
Tone Berg <berton15@student.westerdals.no>
2015-12-13 12:54:01 +0100

    Test createAdjectiveStory
    Simple recorded test.

---
+++ StoryCreatorTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class StoryCreatorTest.
+ *
+ * @author  Tone Berg
+ * @version 2015.12.13
+ */
+public class StoryCreatorTest
+{
+    /**
+     * Default constructor for test class StoryCreatorTest
+     */
+    public StoryCreatorTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void createAdjectiveStory()
+    {
+        StoryCreator storyCre2 = new StoryCreator();
+        storyCre2.createAdjectiveStory("adjektivhistorie.txt", "adjektiv.txt", "ferdig.txt");
+    }
+}
+
